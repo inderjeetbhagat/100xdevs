14a7cd4cb48a227807e68a60b8e77eb1e9e7c991
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-01-22 03:30:54 +0530

    counter using react state

---
+++ week-5/class-5-1/counterUsingReact/counter-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState} from 'react';
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  function onClickHandler() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <button onClick = {onClickHandler}> Counter {count}</button>
+    </div>
+  )
+}
+
+export default App
