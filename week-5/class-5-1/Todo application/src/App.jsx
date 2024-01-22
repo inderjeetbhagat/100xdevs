308ffab276f085d138993099a2737f6269dc67e2
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-01-22 13:46:41 +0530

    Simple Todo using React

---
+++ week-5/class-5-1/Todo application/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([{
+    title: "Go To Gym",
+    description: "Go from 9-5",
+    completed: false
+  },{
+    title: "Study DSA",
+    description: "Do Striver Sheet",
+    completed: true
+  }]);
+
+  function addTodo(){
+    setTodos([...todos, {
+      title: "new Todo",
+      description: " Hi there"
+    }])
+  }
+
+  return (
+  <div>
+    <button onClick={addTodo}>Add a Random TODO</button>
+    {todos.map(function(todo) {
+      return <Todo title={todo.title} description={todo.description}/>
+    })}
+    
+  </div>
+  )
+}
+
+function Todo(props) {
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+
+  </div>
+}
+
+export default App
