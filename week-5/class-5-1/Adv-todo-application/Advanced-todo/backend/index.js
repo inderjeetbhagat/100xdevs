d5b9fb1fcaffc5c84406415f2cf0414a1ff98ca7
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-01-28 17:54:16 +0530

    Advance Todo Application types done

---
+++ week-5/class-5-1/Adv-todo-application/Advanced-todo/backend/index.js
@@ -0,0 +1,20 @@
+//write basic express boilerplate code
+//with express.json() middleware
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+
+
+app.post("/todo", function(req, res){
+    
+})
+app.get("/todos", function(req, res){ 
+
+})
+app.put("/completed", function(req, res){
+
+})
+
